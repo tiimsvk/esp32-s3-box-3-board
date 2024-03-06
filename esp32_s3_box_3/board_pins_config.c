b0f69e98e71827ac93a03d5b80e948e7f4637266
tiimsvk <52893640+tiimsvk@users.noreply.github.com>
2024-03-06 16:04:23 +0100

    Update board_pins_config.c

--- esp32_s3_box_3/board_pins_config.c
+++ esp32_s3_box_3/board_pins_config.c
@@ -56,7 +56,7 @@ esp_err_t get_i2s_pins(i2s_port_t port, i2s_pin_config_t *i2s_config) {
     i2s_config->bck_io_num = GPIO_NUM_1;
     i2s_config->ws_io_num = GPIO_NUM_2;
     i2s_config->data_out_num = GPIO_NUM_40;
-    i2s_config->data_in_num = GPIO_NUM_16;
+    i2s_config->data_in_num = GPIO_NUM_41;
     i2s_config->mck_io_num = GPIO_NUM_2;	
   } else {
     memset(i2s_config, -1, sizeof(i2s_pin_config_t));

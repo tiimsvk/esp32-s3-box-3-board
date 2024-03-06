53333d71331637b25b9eaf0d0097fdda339a5f40
tiimsvk <52893640+tiimsvk@users.noreply.github.com>
2024-03-06 15:35:04 +0100

    Update board_pins_config.c

--- esp32_s3_box_3/board_pins_config.c
+++ esp32_s3_box_3/board_pins_config.c
@@ -53,8 +53,8 @@ esp_err_t get_i2c_pins(i2c_port_t port, i2c_config_t *i2c_config) {
 esp_err_t get_i2s_pins(i2s_port_t port, i2s_pin_config_t *i2s_config) {
   AUDIO_NULL_CHECK(TAG, i2s_config, return ESP_FAIL);
   if (port == I2S_NUM_0) {
-    i2s_config->bck_io_num = GPIO_NUM_02;
-    i2s_config->ws_io_num = GPIO_NUM_01;
+    i2s_config->bck_io_num = GPIO_NUM_2;
+    i2s_config->ws_io_num = GPIO_NUM_1;
     i2s_config->data_out_num = GPIO_NUM_40;
     i2s_config->data_in_num = GPIO_NUM_16;
     i2s_config->mck_io_num = GPIO_NUM_2;	
